78bce9d2c1a3178b03932eccf473aedc63c5c02f
LTAG Developer <leonardocastro.consultor@gmail.com>
2024-07-03 20:01:33 -0300

    feat: setup google maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:location/features/maps/ui/pages/map_view_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
+  await FlutterConfig.loadEnvVariables();
   runApp(const MainApp());
 }
 
@@ -10,11 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: MapViewPage(),
     );
   }
 }
